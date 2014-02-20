2b2099ac03bbb9e0416ca47cb51b84183ff2ea04
anna <anna@gedeon>
2014-02-20 22:31:27 +0100

    Backend skeleton works
    Added Datanucleus "Unowned" annotations
    to the Exercise->Stylesheet and Exercise->Template
    relations. Now Exercises are saved.
    However, there are still problems with the styling.

--- rt4/src/main/java/com/ck/rt1/model/Exercise.java
+++ rt4/src/main/java/com/ck/rt1/model/Exercise.java
@@ -3,9 +3,12 @@ package com.ck.rt1.model;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.tostring.RooToString;
+
+import com.google.appengine.datanucleus.annotations.Unowned;
 
 @RooJavaBean
 @RooToString
@@ -17,10 +20,12 @@ public class Exercise {
     @Size(max = 64000)
     private String content;
 
+    @Unowned
     @NotNull
     @ManyToOne
     private Stylesheet stylesheet;
 
+    @Unowned
     @NotNull
     @ManyToOne
     private Template template;
